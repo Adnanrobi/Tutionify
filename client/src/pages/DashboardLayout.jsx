efcef118e8d5cf61f6fc796317f258617e1085f8
Adnanrobi <adnanrobi6@gmail.com>
2023-12-30 13:22:12 +0600

    -Created components for dashboard

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
 const DashboardLayout = () => {
   return (
-    <div>DashboardLayout</div>
-  )
-}
-export default DashboardLayout
+    <div>
+      DashboardLayout
+      <Outlet />
+    </div>
+  );
+};
+export default DashboardLayout;
