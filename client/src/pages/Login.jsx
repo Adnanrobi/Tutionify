37e36f990f9c6b85f24cb89e764cb8dbbb33af6d
Adnanrobi <adnanrobi6@gmail.com>
2023-12-30 13:04:40 +0600

    -done with login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <Wrapper>
+      <div className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="text" name="email" defaultValue="adnanrobi@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="test123" />
+        <button className="btn btn-block">Login</button>
+        <button className="btn btn-block">Explore The App</button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register here
+          </Link>
+        </p>
+      </div>
+    </Wrapper>
+  );
+};
+export default Login;
