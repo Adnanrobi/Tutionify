79533bfdfaa2b84586fc81e0a549e248045c44e9
Adnanrobi <adnanrobi6@gmail.com>
2023-12-31 02:24:46 +0600

    -sidebar added - both small and big screen handled with separated files

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,6 +1,26 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
-
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+import { useDashboardContext } from "../pages/DashboardLayout";
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { showSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+        </div>
+        <div className="nav-link">
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default BigSidebar;
